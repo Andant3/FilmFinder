2600f315bb0bab47784ee9569ebdfb8426a4ada2
Andant3 <kamikadze3571@gmail.com>
2025-01-06 19:57:57 +0100

    Providing Retrofit

---
+++ app/src/main/java/com/example/filmfinder/retrofit/Movie.kt
@@ -0,0 +1,21 @@
+package com.example.filmfinder.retrofit
+
+import com.google.gson.annotations.SerializedName
+import java.util.Date
+
+data class Movie(
+    @SerializedName("adult")
+    val adultRating: Boolean,
+    @SerializedName("backdrop_path")
+    val backDropPath: String,
+    @SerializedName("genre_ids")
+    val genreIDs: List<Int>,
+    val title: String,
+    val overview: String,
+    @SerializedName("poster_path")
+    val movieImg: String,
+    @SerializedName("release_date")
+    val releaseDate: Date,
+    @SerializedName("vote_average")
+    val rating: Double
+)
---
+++ app/src/main/java/com/example/filmfinder/retrofit/RetrofitInstance.kt
@@ -0,0 +1,18 @@
+package com.example.filmfinder.retrofit
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+    val api: ServiceTMDB by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(ServiceTMDB::class.java)
+    }
+}
